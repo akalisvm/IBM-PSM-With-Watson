88834a2a9196ab88ff21e3a3e7956a834b321105
akalivm <2722968218@qq.com>
2022-10-31 21:37:36 +0000

    create a backbone of the project

---
+++ springboot/src/main/java/ucl/ac/uk/ibmpsmwithwatson/IbmPsmWithWatsonApplication.java
@@ -0,0 +1,13 @@
+package ucl.ac.uk.ibmpsmwithwatson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmPsmWithWatsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmPsmWithWatsonApplication.class, args);
+	}
+
+}
